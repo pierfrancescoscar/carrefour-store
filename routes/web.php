0b35ee4b1067c0c69ea7c3f16efca2e8de975df0
LuigiRagusa95 <luigiragusa99@gmail.com>
2022-02-02 17:53:59 +0100

    add auth and home controller

--- routes/web.php
+++ routes/web.php
@@ -1,22 +1,17 @@
 <?php
 
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
-
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', 'HomeController@index');
 
 Auth::routes();
 
-Route::get('/home', 'HomeController@index')->name('home');
+// Route::get('/home', 'HomeController@index')->name('home');
+Route::middleware('auth')->namespace('Admin')->name('admin.')->prefix('admin')->group(function () {
+    Route::get('/', 'HomeController@index')->name('home');
+});
+
+Route::get('{any?}', function () {
+    return view('guest.home');
+})->where('any', '.*');
