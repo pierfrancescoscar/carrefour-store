7d76b72a90dc3016c680613613def61b9957cda5
luirusso <luirusso_@hotmail.it>
2022-02-02 17:08:20 +0100

    created seeder and verified that it is working

--- database/seeds/ProductsTableSeeder.php
+++ database/seeds/ProductsTableSeeder.php
@@ -1,6 +1,9 @@
 <?php
 
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Str;
+use App\Product;
+
 
 class ProductsTableSeeder extends Seeder
 {
@@ -11,6 +14,16 @@ class ProductsTableSeeder extends Seeder
      */
     public function run()
     {
-        //
+        $products = config('product-data');
+        foreach ($products as $product) {
+            $new_product = new Product();
+
+            $new_product->name = $product['name'];
+            $new_product->price = $product['price'];
+            $new_product->description = $product['description'];
+            $new_product->available = $product['available'];
+
+            $new_product->save();
+        }
     }
 }
