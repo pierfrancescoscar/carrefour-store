6307217d4cc1beed9153f1df059f83f79f7e6399
LuigiRagusa95 <luigiragusa99@gmail.com>
2022-02-03 18:07:16 +0100

    ADD: table, change the type of price, show content

--- resources/views/admin/products/index.blade.php
+++ resources/views/admin/products/index.blade.php
@@ -1,25 +1,42 @@
+@extends('layouts.app')
 
-@extends('') {{--YEALD --}}
 
 
-	
-@section('main-section')
-<section class="products">
-	<div class="container">
-		<ul class="products__list">
-				<li  class=" products__item">
-						<h2 class="products__title">SALAMI</h2>
-						<div class="products__poster">
-							<img src="https://picsum.photos/100/100" alt="">   
-						</div>
-						<div class="products__price">2455$</div>
-						<div class="products-control">
-							<button class="products-control__remove btn"><i class="fas fa-trash-alt"></i></button>
-							<button class="products-control__edit btn" ><i class="fas fa-edit"></i></button>
-							<button class="products-control__show btn"><i class="fas fa-eye"></i></button>
-						</div>
-				</li>
-		</ul>
+@section('content')
+<div class="container">
+	<div class="row">
+		<div class="col">
+			<table class="table table-bordered">
+				<thead>
+					<tr>
+						<th scope="col">ID</th>
+						<th scope="col">Name</th>
+						<th scope="col">Price</th>
+						<th scope="col">Description</th>
+						<th scope="col">Available</th>
+						<th scope="col">Actions</th>
+					</tr>
+				</thead>
+				<tbody>
+					@foreach ($products as $product)
+					<tr>
+						<td>{{$product->id}}</td>
+						<td>{{$product->name}}</td>
+						<td>{{number_format($product->price, 2)}}</td>
+						<td>{{$product->description}}</td>
+						<td>{{$product->available}}</td>
+						<td colspan="3">
+							<ul class="d-flex m-0 p-0 list-style-none">
+								<li class=""><a href="#" class="nav-link ms-1 p-0">Edit</a></li>
+								<li class="ml-2"><a href="#" class="nav-link ms-1 p-0">Update</a></li>
+								<li class="ml-2"><a href="#" class="nav-link ms-1 p-0">Delete</a></li>
+							</ul>
+						</td>
+					</tr>
+					@endforeach
+				</tbody>
+			</table>
+		</div>
 	</div>
-</section>
+</div>
 @endsection
