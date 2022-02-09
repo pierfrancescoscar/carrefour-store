cb390105254c65f4d1112473c6b0b8d8a4c8adc3
LuigiRagusa95 <luigiragusa99@gmail.com>
2022-02-09 16:04:44 +0100

    FIX: guest navbar

---
+++ resources/views/components/navbar.blade.php
@@ -0,0 +1,22 @@
+<div class="custom-header">
+    {{-- LOGO --}}
+    <div class="custom-header__logo">
+        <img src="{{ asset('images/carrefour-logomark.svg') }}" alt="">
+        <img src="{{ asset('images/carrefour-logotype.svg') }}" alt="">
+    </div>
+    {{-- INPUT --}}
+    <div class="custom-header__form">
+        <form action="">
+            <input placeholder="Di cosa hai bisogno?" class="custom-header__input pl-4" type="text">
+            <button class="search-btn"><i class="fas fa-search"></i></button>
+        </form>
+    </div>
+    <div class="pay-block">
+        <div class="custom-header__payback payback">
+            <h3>payback</h3>
+            <h4>Scopri Payback</h4>
+            <img src="{{ asset('images/payback-ball.png') }}" alt="">
+        </div>
+        <a href=""><i class="fas fa-shopping-cart"></i></a>
+    </div>
+</div>
