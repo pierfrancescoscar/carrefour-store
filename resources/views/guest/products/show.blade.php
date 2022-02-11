169f7a624a7af92e0dea5ee6621e72c580dbcbf7
LuigiRagusa95 <luigiragusa99@gmail.com>
2022-02-11 16:19:01 +0100

    COMPLETE: all the styles

--- resources/views/guest/products/show.blade.php
+++ resources/views/guest/products/show.blade.php
@@ -29,11 +29,28 @@
                 <div class="d-flex flex-column">
                     <button class="btn-icon"><i data-feather="heart"></i></button>
                     <div class="d-block mt-4">
-                        <h3 class="text-20">{{ $product->name }}</h3>
-                        <p class="text-14">{{ $product->description }}</p>
+                        <h3 class="text-20 fw-bold">{{ $product->name }}</h3>
+                        <p class="text-14 fw-bolder">{{ $product->description }}</p>
                     </div>
                 </div>
-                <div class="d-flex">1</div>
+                <div class="d-flex align-items-end justify-content-between mt-5">
+                    <div class="d-flex flex-column  mt-3">
+                        <div class="d-flex text-14 my-2">
+                            @if($product->available == 1)
+                            <span class="d-flex text-14 text-green">Disponibile</span>
+                            @else
+                            <span class="d-flex text-14 text-red">Non Disponibile</span>
+                            @endif
+                        </div>
+                        <div class="d-flex text-20 fw-bold">{{ '€ ' . number_format($product->price, 2) }}</div>
+                    </div>
+                    <div class="d-flex">
+                        <button class="d-flex btn-shop">
+                            <span class="mr-3 text-16">Aggiungi</span>
+                            <span><i data-feather="shopping-cart"></i></span>
+                        </button>
+                    </div>
+                </div>
             </div>
         </div>
     </div>
