4e0f9c5b83b62c8c8511b5705c3bf85b863ec981
Yu4iy <yurimohyliuk@gmail.com>
2022-02-15 18:59:59 +0100

    ADD: footer links

--- resources/views/components/footer.blade.php
+++ resources/views/components/footer.blade.php
@@ -1,4 +1,5 @@
 <footer>
+
 	<div class="social">
 		<h4>#restaconnesso</h4>
 	 <i class="fab fa-facebook-f"></i>
@@ -7,6 +8,137 @@
 	 <i class="fab fa-youtube"></i>
 	 <i class="fab fa-linkedin-in"></i>
 	 </div>
+
+	 <div class="footer-wrppaer">
+		 <div class="container">
+			 <div class="footer-links">
+				<div class="footer-column">
+					<ul>
+						<li><h4>I nostri valori</h4></li>
+						<li><a href="">Act For Food</a></li>
+					</ul>
+					<ul>
+						<li><h4>Nel mondo</h4></li>
+						<li><a href="">www.carrefour.com</a></li>
+					</ul>
+
+					<ul>
+						<li><h4></h4></li>
+						<li><a href="">Azienda</a></li>
+						<li><a href="">Ufficio Stampa</a></li>
+						<li><a href="">Lavora con noi</a></li>
+						<li><a href="">Franchising</a></li>
+					</ul>
+
+					<ul>
+						<li><h4>Servizi</h4></li>
+						<li><a href="">Carta SpesAmica PAYBACK</a></li>
+						<li><a href="">Gift card</a></li>
+						<li><a href="">Servizi Findomestic</a></li>
+						<li><a href="">Carrefour Salute</a></li>
+						<li><a href="">Carrefour Viaggi</a></li>
+						<li><a href="">Carrefour Foto</a></li>
+						<li><a href="">Richiamo prodotti</a></li>
+					</ul>
+				</div>
+				<div class="footer-column">
+					<ul>
+						<li><h4>Area legale</h4></li>
+						<li><a href="">Dati societari</a></li>
+						<li><a href="">Tutela dei dati personali</a></li>
+						<li><a href="">Garanzia legale di conformità</a></li>
+						<li><a href="">Condizioni generali</a></li>
+						<li><a href="">Condizioni generali di vendita</a></li>
+						<li><a href="">Carta etica per i nostri fornitori</a></li>
+						<li><a href="">Carta di impegno di Responsabilità</a></li>
+						<li><a href="">Sociale d'Impresa</a></li>
+						<li><a href="">Modelli Organizzativi</a></li>
+						<li><a href="">Informativa Cookie</a></li>
+						<li><a href="">Gestisci preferenze cookie</a></li>
+					</ul>
+					
+					<ul>
+						<li><h4>Acquisti Online</h4></li>
+						<li><a href="">Spesa veloce by Carrefour NOW</a></li>
+						<li><a href="">Spesa Online</a></li>
+						<li><a href="">Elettronica e Casa</a></li>
+						<li><a href="">Offerte Online</a></li>
+						<li><a href="">Abbonamento Carrefour Club</a></li>
+						<li><a href="">Pagamento tramite Gift Card</a></li>
+						<li><a href="">Soglie spese di spedizione</a></li>
+						<li><a href="">Novità servizi spesa online</a></li>
+						<li><a href="">App Carrefour</a></li>
+						<li><a href="">Tutti i servizi online</a></li>
+					</ul>
+				</div>
+				<div class="footer-column">
+					<ul>
+						<li><h4>Agevolazioni</h4></li>
+						<li><a href="">Donne in Gravidanza e Neogenitori</a></li>
+						<li><a href="">Over 60</a></li>
+						<li><a href="">Diversamente Abili</a></li>
+						<li><a href="">Regolamento agevolazioni</a></li>
+					</ul>
+					<ul>
+						<li><h4>Speciali del momento</h4></li>
+						<li><a href="">Le Nostre Proposte di Spesa Online</a></li>
+						<li><a href="">Le Nostre Proposte di Elettronica e Casa</a></li>
+						<li><a href="">Carnevale 2022</a></li>
+						<li><a href="">Prodotti Biologici Online</a></li>
+						<li><a href="">Prodotti Senza Glutine</a></li>
+						<li><a href="">Prodotti Senza Lattosio</a></li>
+						<li><a href="">Prodotti Vegani</a></li>
+						<li><a href="">Prodotti Vegetariani</a></li>
+						<li><a href="">Prodotti Proteici</a></li>
+						<li><a href="">Prodotti Senza Zucchero</a></li>
+						<li><a href="">Vini Consigliati</a></li>
+						<li><a href="">Giocattoli Online</a></li>
+						<li><a href="">Idee Regalo da Acquistare Online</a></li>
+						<li><a href="">Elettrodomestici Samsung</a></li>
+						<li><a href="">Smart Tv e Televisori</a></li>
+						<li><a href="">Smartphone e Cellulari</a></li>
+						<li><a href="">Tutti gli Speciali del Momento</a></li>
+					</ul>
+				</div>
+			 </div>
+
+			 <div class="footer-customer-care">
+					<div class="footer-customer-care__call-us">
+						<div class="logo">
+							<img src="https://www.carrefour.it/on/demandware.static/-/Library-Sites-carrefour-library-IT/default/dwd3d279b3/call-us.png" alt="">
+						</div>
+						<div class="contact">
+							<h5>CHIAMACI</h5>
+							<div>all numero verde</div>
+							<div class="cell">800 650 950</div>
+						</div>
+					</div>
+					<div class="footer-customer-care__wrapper">
+						<div class="footer-customer-care__item">
+							<img src="https://www.carrefour.it/on/demandware.static/-/Library-Sites-carrefour-library-IT/default/dw680dbb0b/point-of-service.png" alt="">
+							<div>Punto di Vendita</div>
+							<h6>Trova un</h6>
+						</div>
+						<div class="footer-customer-care__item">
+							<img src="https://www.carrefour.it/on/demandware.static/-/Library-Sites-carrefour-library-IT/default/dw73041029/contact-service.png" alt="">
+							<div> Contatta il</div>
+							<h6>Servizio clienti</h6>
+						</div>
+					</div>
+			 </div>
+		 </div>
+	 </div>
+
+	 <div class="pay">
+		<div class="pay__title">Metodi di pagamento</div>
+		<ul class="pay__icon">
+			<img class="pay__visa" src="../images/visa.svg" alt="">
+			<img class="pay__mc" src="../images/mastercard.svg" alt="">
+			<img class="pay__express" src="../images/american_express.svg" alt="">
+			<img class="pay__pp" src="../images/paypal.svg" alt="">
+		</ul>
+	</div>
+
 	<div class="made">
 		 <h5>Made with out <i class="fas fa-heart-broken"></i> by Team JavaScript </h5>
 	</div>
