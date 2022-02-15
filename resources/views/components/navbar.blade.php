518e7dc1cacaf5398f156ae064f3aa7359b49097
Yu4iy <yurimohyliuk@gmail.com>
2022-02-15 16:37:10 +0100

    FIX: header

--- resources/views/components/navbar.blade.php
+++ resources/views/components/navbar.blade.php
@@ -1,3 +1,4 @@
+<div class="container">
 <div class="custom-header">
     {{-- LOGO --}}
     <div class="custom-header__logo">
@@ -5,12 +6,10 @@
         <img src="{{ asset('images/carrefour-logotype.svg') }}" alt="">
     </div>
     {{-- INPUT --}}
-	 <form action="">
-			<div class="custom-header__form">
+	 <div class="custom-header__form">
             <input placeholder="Di cosa hai bisogno?" class="custom-header__input pl-4" type="text">
             <button class="search-btn"><i class="fas fa-search"></i></button>
-			</div>
-        </form>
+		</div>
     <div class="pay-block">
         <div class="custom-header__payback payback">
             <h3>payback</h3>
@@ -19,4 +18,22 @@
         </div>
         <a href=""><i class="fas fa-shopping-cart"></i></a>
     </div>
+</div>
+<div class="navbar">
+
+		<ul class="navbar__list">
+			<li>Spesa Online</li>
+			<li>Elettronica e Giocattoli <i class="fas fa-caret-down"></i></li>
+			<li>Promozioni</li>
+		</ul>
+
+	
+		<ul class="navbar__list">
+			<li>Vivi di Gusto</li>
+			<li>Idee regalo <i class="fas fa-caret-down"></i></li>
+			<li>Spesa Smart <i class="fas fa-caret-down"></i></li>
+			<li><i class="fas fa-user"></i> Accedi <i class="fas fa-caret-down"></i></li>
+		</ul>
+
+</div>
 </div>
