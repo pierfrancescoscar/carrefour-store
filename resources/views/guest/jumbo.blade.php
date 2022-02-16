751f9a1aa50a39fff8660f3b22c6376da3a2f891
lnannettik <l.nannetti.k@gmail.com>
2022-02-16 17:51:06 +0100

    struttura carousel

--- resources/views/guest/jumbo.blade.php
+++ resources/views/guest/jumbo.blade.php
@@ -9,16 +9,18 @@
     <div class="row">
 
         <div class="d-flex">
+
+            {{-- PAGINA 1 --}}
             <div class="col-12 carusel_container">
 
                 <ul class="d-flex carusel">
                     <li class="left_side col-6 d-flex flex-column">
                         <div class="img_container m-2">
-                            <img src="https://www.carrefour.it/on/demandware.static/-/Sites/default/dw796522af/banner/main-carousel/food/banner_piccolo/risparmio-di-qualita/Bannerpiccolo_mob_02.jpg" alt="">
+                            <img src="https://www.carrefour.it/on/demandware.static/-/Sites/default/dw796522af/banner/main-carousel/food/banner_piccolo/risparmio-di-qualita/Bannerpiccolo_mob_02.jpg" alt="Risparmio">
                         </div>
 
                         <div class="img_container m-2">
-                            <img src="https://www.carrefour.it/on/demandware.static/-/Sites/default/dw783ba8e5/banner/main-carousel/food/banner_piccolo/raddoppia-e-risparmia-feb-2022/R_R_Bannerpiccolo_mob_.jpg" alt="">
+                            <img src="https://www.carrefour.it/on/demandware.static/-/Sites/default/dw783ba8e5/banner/main-carousel/food/banner_piccolo/raddoppia-e-risparmia-feb-2022/R_R_Bannerpiccolo_mob_.jpg" alt="Raddoppia">
                         </div>
                     </li>
 
@@ -28,38 +30,62 @@
                         </div>
 
                         <div class="img_container m-2">
-                            <img src="https://www.carrefour.it/on/demandware.static/-/Sites/default/dwcf5ee6bb/banner/main-carousel/food/placeholder/pasticceria/Pasticceria_Placeholder-Bannerpiccolo_mob.jpg" alt="">
+                            <img src="https://www.carrefour.it/on/demandware.static/-/Sites/default/dwcf5ee6bb/banner/main-carousel/food/placeholder/pasticceria/Pasticceria_Placeholder-Bannerpiccolo_mob.jpg" alt="Pasticcieria">
                         </div>
                     </li>
                 </ul>
-                
             </div>
 
+            {{-- PAGINA 2 --}}
             <div class="col-12 carusel_container">
 
                 <ul class="d-flex carusel">
                     <li class="left_side col-6 d-flex flex-column">
                         <div class="img_container m-2">
-                            <img src="https://www.carrefour.it/on/demandware.static/-/Sites/default/dw796522af/banner/main-carousel/food/banner_piccolo/risparmio-di-qualita/Bannerpiccolo_mob_02.jpg" alt="">
+                            <img src="https://www.carrefour.it/on/demandware.static/-/Sites/default/dw8b4d291e/banner/main-carousel/food/banner_piccolo/colazione-barilla/ColazioneCollectionbannerHPmobile-768x200.jpg" alt="Colazione-Barilla">
                         </div>
 
                         <div class="img_container m-2">
-                            <img src="https://www.carrefour.it/on/demandware.static/-/Sites/default/dw783ba8e5/banner/main-carousel/food/banner_piccolo/raddoppia-e-risparmia-feb-2022/R_R_Bannerpiccolo_mob_.jpg" alt="">
+                            <img src="https://www.carrefour.it/on/demandware.static/-/Sites/default/dwe8125c83/banner/main-carousel/food/banner_piccolo/carnevale/Carnevale-Bannerpiccolo_mob.jpg" alt="Carnevale">
                         </div>
                     </li>
 
                     <li class="right_side col-6 d-flex flex-column">
                         <div class="img_container m-2">
-                            <img src="https://www.carrefour.it/on/demandware.static/-/Sites/default/dw3ebf0874/banner/main-carousel/food/banner_piccolo/abinbev/03_SESTINI_MULTIBRAND_STD_CARREFOUR_CAROUSEL_MB_768x200_b.jpg" alt="">
+                            <img src="https://www.carrefour.it/on/demandware.static/-/Sites/default/dw40b5910a/banner/main-carousel/food/banner_piccolo/l-oreal-paris/banner-mob.jpg" alt="l-oreal-paris">
                         </div>
 
                         <div class="img_container m-2">
-                            <img src="https://www.carrefour.it/on/demandware.static/-/Sites/default/dwcf5ee6bb/banner/main-carousel/food/placeholder/pasticceria/Pasticceria_Placeholder-Bannerpiccolo_mob.jpg" alt="">
+                            <img src="https://www.carrefour.it/on/demandware.static/-/Sites/default/dwe593ac51/banner/main-carousel/food/banner_piccolo/scJohnson/Glade_PromoCard_LoveTheSign_WEB_993x275_B0222.jpg" alt="sc-Johnson">
                         </div>
                     </li>
                 </ul>
-                
+                                
             </div>
+
+            {{-- PAGINA 3 --}}
+            <div class="col-12 carusel_container">
+
+                <ul class="d-flex carusel">
+                    <li class="left_side col-6 d-flex flex-column">
+                        <div class="img_container m-2">
+                            <img src="https://www.carrefour.it/on/demandware.static/-/Sites/default/dw58af5af9/banner/main-carousel/food/banner_piccolo/danome-actimel/E_CRF_ACTIMEL_FEB_HP_Mobile_768x200px.jpg" alt="Danone-actimel">
+                        </div>
+
+                        <div class="img_container m-2">
+                            <img src="https://www.carrefour.it/on/demandware.static/-/Sites/default/dwc4c591fc/banner/main-carousel/food/banner_piccolo/capitan-findus/HOMEPAGE_MOBILE_768X200_2.jpg" alt="Capitan-Findus">
+                        </div>
+                    </li>
+
+                    <li class="right_side col-6 d-flex flex-column">
+                        <div class="img_container m-2">
+                            <img src="https://www.carrefour.it/on/demandware.static/-/Sites/default/dwc815521a/banner/main-carousel/food/placeholder/terre-d-italia/Bannergrande_mob_02.jpg" alt="Terre-d-Italia">
+                        </div>
+                    </li>
+                </ul>
+
+            </div>
+            
         </div>
     </div>
 </div>
